d36ebd117e7190bb09fb096fcd06c351630d106a
codegrann <omondihills02@gmail.com>
2023-08-21 17:01:32 +0300

    .

--- social-dapp-contract/hardhat.config.js
+++ social-dapp-contract/hardhat.config.js
@@ -3,4 +3,11 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: "YOUR_INFURA_GOERLI_URL",
+      accounts: [0a2f7c20c19c0cd313a4957c2280496423fdccd82ad5ca4aa99bcb9792c7cf4e],
+    },
+    // Add more networks as needed
+  },
 };
