6299c941b8231d51f575580cd7f168be7c62a863
codegrann <omondihills02@gmail.com>
2023-08-21 16:05:07 +0300

    .

--- social-dapp-contract/scripts/deploy.js
+++ social-dapp-contract/scripts/deploy.js
@@ -6,26 +6,43 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// scripts/deploy.js
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  const [deployer] = await ethers.getSigners();
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const SocialDapp = await ethers.getContractFactory("SocialDapp");
+  const socialDapp = await SocialDapp.deploy();
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log("SocialDapp address:", socialDapp.address);
 }
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+// const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
+// const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+//
+// const lockedAmount = hre.ethers.utils.parseEther("1");
+//
+// const Lock = await hre.ethers.getContractFactory("Lock");
+// const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+//
+// await lock.deployed();
+//
+// console.log(
+// `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+// );
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+// console.error(error);
+// process.exitCode = 1;
+// });
