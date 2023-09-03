565a308decab3b013189958450aede3da65267ec
codegrann <omondihills02@gmail.com>
2023-09-03 10:49:58 +0300

    .

--- social-dapp-contract/hardhat.config.js
+++ social-dapp-contract/hardhat.config.js
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: "YOUR_INFURA_GOERLI_URL",
+      url: "https://rinkeby.infura.io/v3/5cf4b03b1e6347e39ced124f8d5f8698",
       accounts: [0a2f7c20c19c0cd313a4957c2280496423fdccd82ad5ca4aa99bcb9792c7cf4e],
     },
     // Add more networks as needed
